5d86d867eca6e361d234677076657604043bd54a
Sagi Cohen <sagidevsage@gmail.com>
2025-11-03 17:53:03 +0200

    added admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $this->call([
+            AdminUserSeeder::class,
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
